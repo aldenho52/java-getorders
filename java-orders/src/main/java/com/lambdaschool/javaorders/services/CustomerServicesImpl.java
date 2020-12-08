be5e72d6df1a95c551e6a7055fc5feba47f46975
aldenho52 <69052933+aldenho52@users.noreply.github.com>
2020-12-08 15:31:12 -0700

    set up files, got customers/orders getMapping working

---
+++ java-orders/src/main/java/com/lambdaschool/javaorders/services/CustomerServicesImpl.java
@@ -0,0 +1,25 @@
+package com.lambdaschool.javaorders.services;
+
+import com.lambdaschool.javaorders.models.Customer;
+import com.lambdaschool.javaorders.repositories.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@Service(value = "customerService")
+public class CustomerServicesImpl implements CustomerServices {
+    @Autowired
+    private CustomersRepository custrepos;
+
+    @Override
+    public List<Customer> findAllCustomers()
+    {
+        List<Customer> rtnList = new ArrayList<>();
+        custrepos.findAll().iterator().forEachRemaining(rtnList::add);
+        return rtnList;
+    }
+}
